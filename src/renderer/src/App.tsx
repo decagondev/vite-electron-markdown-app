ab5d281edfb70e304fa9824ee7706c30261be328
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-18 17:31:35 +0000

    updated app

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useCallback } from "react"
 import Editor from "./components/Editor"
 import Preview from "./components/Preview"
-
+import './app.css'
 function App(): JSX.Element {
   const [doc, setDoc] = useState<string>('# Hello, World!\n')
 
@@ -9,7 +9,7 @@ function App(): JSX.Element {
     setDoc(newDoc)
   }, [])
   return (
-    <div className="container">
+    <div className="app">
       <Editor onChange={handleDocChange} initialDoc={doc} />
       <Preview doc={doc} />
     </div>
